9599f0fa6a5cd78813abffca2de6409325b7a3d9
Amy <amruta.ghate@gmail.com>
2015-06-28 19:39:01 -0400

    6282015

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,17 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+   
+
+    return move||getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+     
+    return move||randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -40,7 +43,23 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove == computerMove){ winner = "tie" }
+
+    if (playerMove == "rock") {
+        if(computerMove == "paper"){ winner = "computer" };
+        if(computerMove == "scissors"){ winner = "player" };
+    };
+
+    if (playerMove == "paper") {
+        if(computerMove == "rock"){ winner = "player" };
+        if(computerMove == "scissors"){ winner = "computer" };
+    };
+
+    if (playerMove == "scissors") {
+        if(computerMove == "paper"){ winner = "player" };
+        if(computerMove == "rock"){ winner = "computer" };   
+    };
+
     return winner;
 }
 
@@ -52,4 +71,3 @@ function playToFive() {
     /* YOUR CODE HERE */
     return [playerWins, computerWins];
 }
-
